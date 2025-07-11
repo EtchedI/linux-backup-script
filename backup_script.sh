672e2694300c0f277cb1e3404fe04bdb1a4fbbd0
EtchedI <unnameddoe@outlook.com>
2025-07-11 17:13:11 -0500

    Add backup script and README

---
+++ backup_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SOURCE="/c/Users/ryclt/Documents/testfolder"
+DEST="/c/Users/ryclt/Backup/backup-$(date +%Y-%m-%d)"
+
+echo "Backing up $SOURCE to $DEST"
+
+cp -r "$SOURCE" "$DEST"
+
+echo "✅ Backup done! See: $DEST"
+read -p "Press Enter to exit"
